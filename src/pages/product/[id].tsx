191803ef29de1d859d34f0847978ed97ee6ecf26
Cristhoffer Nunes <cristhoffer.nunes.santos@gmail.com>
2025-06-06 21:46:21 -0300

    feat: add product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,23 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../product";
 
 export default function Products() {
   const { query } = useRouter();
 
-  return <h1>{JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta 1</h1>
+        <span>R$ 79,00</span>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor
+          molestiae illum voluptas explicabo est suscipit ut quibusdam vero
+          molestias provident officia enim, blanditiis modi? Amet aperiam
+          quaerat odio eius perspiciatis.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
