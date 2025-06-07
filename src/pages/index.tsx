bea8a7fbfad1b3353ca8069f2acbecf228796889
Cristhoffer Nunes <cristhoffer.nunes.santos@gmail.com>
2025-06-06 22:38:17 -0300

    feat: disable prefetch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,12 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Link key={product.id} href={`/product/${product.id}`} passHref>
+          <Link
+            key={product.id}
+            href={`/product/${product.id}`}
+            passHref
+            prefetch={false}
+          >
             <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
               <footer>
